310d9769beccc357645e765c19510d24a959b3fe
mok <mok0230@protonmail.com>
2021-11-03 11:54:18 -0700

    update test to win game 3

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,4 +1,5 @@
 const { assert } = require("chai");
+// const { ethers } = require("ethers");
 
 describe("Game3", function() {
   it("should be a winner", async function() {
@@ -12,14 +13,20 @@ describe("Game3", function() {
     // hardhat will create 10 accounts for you by default
     // you can get one of this accounts with ethers.provider.getSigner
     // and passing in the zero-based indexed of the signer you want:
-    const signer = ethers.provider.getSigner(0);
-    const address = await signer.getAddress();
+    const signer0 = await ethers.provider.getSigner(0);
+    const signer1 = await ethers.provider.getSigner(1);
+    const signer2 = await ethers.provider.getSigner(2);
 
-    // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: "1" });
+
+    await game.connect(signer0).buy({ value: "2" });
+    await game.connect(signer1).buy({ value: "3" });
+    await game.connect(signer2).buy({ value: "1" });
+
+    const [ address0, address1, address2 ] = await ethers.provider.listAccounts();
+
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(address0, address1, address2);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
