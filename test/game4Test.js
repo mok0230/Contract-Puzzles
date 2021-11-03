8d408138fc4d8c7e06061d7e7a06954771c9b356
mok <mok0230@protonmail.com>
2021-11-03 11:58:54 -0700

    update test to win game 4

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,9 +6,17 @@ describe("Game4", function() {
     const game = await Game.deploy();
     await game.deployed();
 
+    const signer0 = await ethers.provider.getSigner(0);
+    const signer1 = await ethers.provider.getSigner(1);
+
+    const address0 = await signer0.getAddress();
+    const address1 = await signer1.getAddress();
+
+    await game.connect(signer0).write(address1);
+    
     // nested mappings are rough :}
 
-    await game.win();
+    await game.connect(signer1).win(address0);
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
