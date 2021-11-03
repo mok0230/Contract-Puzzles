22dcbcf1c78e377d70b967454f7af47dcd4ca639
mok <mok0230@protonmail.com>
2021-11-03 12:37:20 -0700

    adjust test to win game 5

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,4 +1,6 @@
 const { assert } = require("chai");
+const { utils } = require("ethers");
+
 
 describe("Game5", function() {
   it("should be a winner", async function() {
@@ -6,9 +8,26 @@ describe("Game5", function() {
     const game = await Game.deploy();
     await game.deployed();
 
-    // good luck
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
 
-    await game.win();
+    let randomWallet = ethers.Wallet.createRandom();
+
+    while(randomWallet.address >= threshold) {
+      randomWallet = ethers.Wallet.createRandom();
+    }
+
+    const defaultSigner = await ethers.provider.getSigner(0);
+
+    const winningSigner = await randomWallet.connect(ethers.provider);
+
+    const tx = {
+      to: winningSigner.address,
+      value: utils.parseEther("1.0")
+    }
+
+    await defaultSigner.sendTransaction(tx);
+
+    await game.connect(winningSigner).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), "You did not win the game");
